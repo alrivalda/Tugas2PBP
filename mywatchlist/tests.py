c4fced42905b363a9fdcfa75961996e866f84bc5
Muhammad Al Rivalda <muhammad.al15@ui.ac.id>
2022-09-22 10:35:37 +0700

    cek

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from urllib import response
+from django.test import TestCase, Client 
 
 # Create your tests here.
+
+class Testingmywatchlist(TestCase):
+    def tes_mywatchlist_showhtml(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEquals(response.status_code,200)
+
+    def tes_mywatchlist_showxmll(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEquals(response.status_code,200)
+    
+    def tes_mywatchlist_showjson(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEquals(response.status_code,200)
