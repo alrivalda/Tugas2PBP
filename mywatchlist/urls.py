293ac458c843420b254053e2d5a891ba0ba08b0a
Muhammad Al Rivalda <muhammad.al15@ui.ac.id>
2022-09-22 10:26:19 +0700

    cek

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from mywatchlist.views import show_mywatchlist, show_xml, show_json, show_json_by_id, show_xml_by_id, show_html
+from mywatchlist.views import  show_xml, show_json, show_json_by_id, show_xml_by_id, show_html
 
 app_name ='watchlist'
 urlpatterns = [
-    path('', show_mywatchlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('html/', show_html, name='show_html'),
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,16 +5,6 @@ from django.http import HttpResponse
 from django.core import serializers
 # Create your views here.
 
-def show_mywatchlist(request):
-    data_barang_mywatchlist = Filmwatchlist.objects.all()
-    context = {
-        'list_barang': data_barang_mywatchlist,
-        'nama': 'Muhammad Al Rivalda',
-        'NPM' : '2106708961'
-    }
-    return render(request, "mywatchlist.html", context)
-
-
 
 def show_xml(request):
     data = Filmwatchlist.objects.all()
